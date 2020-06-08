ced8d72ca47767404c638ac9dfa10b0c66317f2e
Ishaan Nerurkar <ishaan.ner@gmail.com>
2020-06-08 15:20:43 +1000

    Updates

---
+++ precache-manifest.1a0e4aa77e591faeeef320fe8d2dcd5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c2d02479061efe3e8564cee5614f82",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "eadb3463fc23e70b9713",
+    "url": "/spotify-clone/static/css/main.c95697b8.chunk.css"
+  },
+  {
+    "revision": "3ea1f03512f024331c91",
+    "url": "/spotify-clone/static/js/2.acd036e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spotify-clone/static/js/2.acd036e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadb3463fc23e70b9713",
+    "url": "/spotify-clone/static/js/main.f33658e4.chunk.js"
+  },
+  {
+    "revision": "bb3f3d08ffea7a70de6f",
+    "url": "/spotify-clone/static/js/runtime-main.f8c82571.js"
+  }
+]);
